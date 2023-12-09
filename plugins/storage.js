4340182ab83257001dfecc13cd9424ba013ff9a6
Dale Zak <dalezak@gmail.com>
2023-12-09 15:56:12 -0600

    Removed plugins/storage

--- plugins/storage.js
+++
@@ -1,5 +0,0 @@
-import { Storage } from '../utils/storage';
-export default defineNuxtPlugin(nuxtApp => {
-  console.log("plugins/storage", process.client ? "client" : "server");
-  nuxtApp.provide('storage', Storage.instance());
-})
