312d4f9bd75a39f8f28c5aae87a2f78196f82ab0
Dale Zak <dalezak@gmail.com>
2023-12-20 07:11:02 -0600

    Updated app.vue

--- app.vue
+++ app.vue
@@ -5,10 +5,4 @@
 </template>
 
 <script setup>
-const storage = useStorage();
-onMounted(async () => {
-  storage.set("supabase", "supabase");
-  let test = await storage.get("supabase");
-  console.log("onMounted supabase", test);
-})
 </script>
