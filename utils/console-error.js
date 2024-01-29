2d5f0b921425fe9a6ed0fd081ba48c52520c21e2
Dale Zak <dalezak@gmail.com>
2024-01-29 07:34:09 -0600

    Updated console utils

--- utils/console-error.js
+++ utils/console-error.js
@@ -1,6 +1,5 @@
 export default async function (message, ...args) {
   if (process.env.NODE_ENV === 'development') {
-    var source = process.client ? "client" : "server";
-    console.error(message, ...args, source);
+    console.error(message, ...args);
   }
 }
--- utils/console-log.js
+++ utils/console-log.js
@@ -1,6 +1,5 @@
 export default async function (message, ...args) {
   if (process.env.NODE_ENV === 'development') {
-    var source = process.client ? "client" : "server";
-    console.log(message, ...args, source);
+    console.log(message, ...args);
   }
 }
