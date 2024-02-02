2d21e5dc6c2c20e89a56ab732021670c114f5b49
Dale Zak <dalezak@gmail.com>
2024-02-02 07:45:41 -0600

    Added console logs

--- composables/useLoggedIn.js
+++ composables/useLoggedIn.js
@@ -1,6 +1,7 @@
 export function useLoggedIn() {
   const loggedIn = ref(false);
   const supabaseUser = useSupabaseUser();
+  consoleLog("useLoggedIn", supabaseUser.value);
   loggedIn.value = supabaseUser.value != null;
   return loggedIn;
 }
